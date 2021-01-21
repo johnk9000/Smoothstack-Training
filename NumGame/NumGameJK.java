678b913717f2677036e2891bba2630802b011b97
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 10:39:18 -0600

    Number guessing game is complete

--- NumGame/NumGameJK.java
+++ NumGame/NumGameJK.java
@@ -1,6 +1,6 @@
 // Java game that user has five tries to guess a number between 1-100 and must be within + or - 10 of the target number
 import java.util.Scanner;
-//package com.ss.basics.numgame;
+// package com.ss.basics.numgame;
 
 public class NumGameJK {
     
@@ -10,11 +10,12 @@ public class NumGameJK {
         int guessNum;
         int count = 0;
         boolean correct = false;
-
+        // Using Scanner util to accept user input to set as guessNum with each iteration
         Scanner input = new Scanner(System.in);
-        //System.out.println(targetNum);
+        
         while(!correct) {
-            System.out.println("Guess number between 1-100: ");
+            System.out.println("Guess a number between 1-100: ");
+            // Setting guessNum as user input after system prompt
             guessNum = input.nextInt();
             //conditionals to check to see if user guess is within the range of +/- 10 of target number
             if(guessNum >= targetNum - 10 && guessNum <= targetNum + 10) {
@@ -22,9 +23,14 @@ public class NumGameJK {
                 correct = true;
             } else {
                 System.out.println("sorry, you have "+(4-count)+" tries remaining.");
-                    System.out.println(count); //debug
-                    System.out.println(targetNum);
+                    //System.out.println(count); //debug
+                    //System.out.println(targetNum); //debug
             count++;
+                // Nested conditional for exceeding five tries which will trigger an exit and reveal the answer
+                if(count > 4) {
+                    System.out.println("Game Over, the number was "+targetNum);
+                    break;
+                }
             }
         }
     }
