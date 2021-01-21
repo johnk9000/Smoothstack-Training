3787090409c361ee5aa3daed4f00391d9dfe5839
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 10:28:36 -0600

    Game works, but can't get OR logic in loop

--- NumGame/NumGameJK.java
+++ NumGame/NumGameJK.java
@@ -11,7 +11,22 @@ public class NumGameJK {
         int count = 0;
         boolean correct = false;
 
-        System.out.println(targetNum);
+        Scanner input = new Scanner(System.in);
+        //System.out.println(targetNum);
+        while(!correct) {
+            System.out.println("Guess number between 1-100: ");
+            guessNum = input.nextInt();
+            //conditionals to check to see if user guess is within the range of +/- 10 of target number
+            if(guessNum >= targetNum - 10 && guessNum <= targetNum + 10) {
+                System.out.println("You are correct!");
+                correct = true;
+            } else {
+                System.out.println("sorry, you have "+(4-count)+" tries remaining.");
+                    System.out.println(count); //debug
+                    System.out.println(targetNum);
+            count++;
+            }
+        }
     }
 
 }
