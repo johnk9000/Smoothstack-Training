eb4374163864130f94120abd880e918d7eba4e09
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 11:16:59 -0600

    First pattern printed

---
+++ PrintPattern/src/ss/basics/PrintPatternJK.java
@@ -0,0 +1,33 @@
+/* 
+@author John Kim
+Java program that will print out various triangle patterns made of asterisks
+*/
+//package com.ss.basics.printpattern;
+
+public class PrintPatternJK {
+    
+    public static void main(String[] args) {
+        // line break var
+        String lineBreak = "----------";
+        // 1) Right triangle
+        for(int i = 0; i < 4; i++) {
+            // printing astericks per row
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println(lineBreak);
+        // 2) Upside-down right triangle
+
+        System.out.println(lineBreak);
+        // 3) Triangle
+
+        System.out.println(lineBreak);
+        // 4) Upside-down triangle
+
+        System.out.println(lineBreak);
+    }
+
+}
+
