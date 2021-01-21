b82d95abbbdbad7ab0afd0d656842cb8a6e11471
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-20 21:17:29 -0600

    Random num gen working

---
+++ NumGame/NumGameJK.java
@@ -0,0 +1,17 @@
+// Java game that user has five tries to guess a number between 1-100 and must be within + or - 10 of the target number
+import java.util.Scanner;
+//package com.ss.basics.numgame;
+
+public class NumGameJK {
+    
+    public static void main(String[] args) {
+        // Declaring variables
+        int targetNum = (int)(Math.random() * 100  + 1);
+        int guessNum;
+        int count = 0;
+        boolean correct = false;
+
+        System.out.println(targetNum);
+    }
+
+}
