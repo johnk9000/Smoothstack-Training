dd9fe20680945d3b05e69631151dced188080401
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 20:37:01 -0600

    Reorganized dirs

--- 01PrintPattern/src/ss/basics/PrintPatternJK.java
+++ 01PrintPattern/src/ss/basics/PrintPatternJK.java
@@ -11,7 +11,7 @@ public class PrintPatternJK {
         String lineBreak = "----------";
         // 1) Right triangle
         for(int i = 0; i < 4; i++) {
-            // printing astericks per row
+            // printing asterisks per row
             for(int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
@@ -20,7 +20,7 @@ public class PrintPatternJK {
         System.out.println(lineBreak);
         // 2) Upside-down right triangle
         for(int i = 0; i < 4; i++) {
-            // printing astericks per row
+            // printing asterisks per row
             for(int j = 4; j > i; j--) {
                 System.out.print("*");
             }
@@ -29,7 +29,7 @@ public class PrintPatternJK {
         System.out.println(lineBreak);
         // 3) Center triangle
         for(int i = 0; i < 4; i++) {
-            // printing astericks per row
+            // printing asterisks per row
             for(int j = 0; j < 9; j++) {
                 if(j >= (4 - i) && j <= (4 + i)) {
                     System.out.print("*");
@@ -42,7 +42,7 @@ public class PrintPatternJK {
         System.out.println(lineBreak);
         // 4) Upside-down center triangle
         for(int i = 0; i < 4; i++) {
-            // printing astericks per row
+            // printing asterisks per row
             for(int j = 0; j < 9; j++) {
                 if(j >= (i + 1) && j <= (7 - i)) {
                     System.out.print("*");
